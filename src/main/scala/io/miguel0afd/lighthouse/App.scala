09cd78cbc9aec6c435fd6421a59610a1ad0b3b17
Miguel Angel Fernandez Diaz <mafernandez@stratio.com>
2015-06-12 20:01:12 +0200

    Adding Spark code

--- src/main/scala/io/miguel0afd/lighthouse/App.scala
+++ src/main/scala/io/miguel0afd/lighthouse/App.scala
@@ -1,5 +1,29 @@
 package io.miguel0afd.lighthouse
 
-class App {
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkContext, SparkConf}
+
+// One method for defining the schema of an RDD is to make a case class with the desired column
+// names and types.
+case class Record(key: Int, value: String)
+
+object App {
+  def main(args: Array[String]) {
+    val sparkConf = new SparkConf().setAppName("Lighthouse")
+    val sc = new SparkContext(sparkConf)
+    val sqlContext = new SQLContext(sc)
+
+    // Importing the SQL context gives access to all the SQL functions and implicit conversions.
+    import sqlContext.implicits._
+
+    val df = sc.parallelize((1 to 100).map(i => Record(i, s"val_$i"))).toDF()
+    // Any RDD containing case classes can be registered as a table.  The schema of the table is
+    // automatically inferred using scala reflection.
+    df.registerTempTable("records")
+
+    // Once tables have been registered, you can run SQL queries over them.
+    println("Result of SELECT *:")
+    sqlContext.sql("SELECT * FROM records").collect().foreach(println)
+  }
 
 }
