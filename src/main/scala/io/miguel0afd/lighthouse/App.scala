73ff4776219ef5725ec5a08d53384ff03d5eb9a1
Miguel Angel Fernandez Diaz <mafernandez@stratio.com>
2015-07-10 08:50:30 +0200

    Some tests added

--- src/main/scala/io/miguel0afd/lighthouse/App.scala
+++ src/main/scala/io/miguel0afd/lighthouse/App.scala
@@ -1,5 +1,6 @@
 package io.miguel0afd.lighthouse
 
+import com.datastax.driver.core.{Row, Session, Cluster}
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkContext, SparkConf}
 
@@ -7,7 +8,7 @@ import org.apache.spark.{SparkContext, SparkConf}
 // names and types.
 case class Record(key: Int, value: String)
 
-object App {
+object MapDataframe {
   def main(args: Array[String]) {
     val sparkConf = new SparkConf().setAppName("Lighthouse").setMaster("local[4]")
     val sc = new SparkContext(sparkConf)
@@ -27,5 +28,81 @@ object App {
 
     sc.stop()
   }
+}
 
+object CassandraReadRDD {
+  def main(args: Array[String]) {
+    val conf = new SparkConf(true).set("spark.cassandra.connection.host", "127.0.0.1")
+
+    val sc = new SparkContext("local[4]", "Lighthouse", conf)
+
+    import com.datastax.spark.connector._
+
+    val rdd = sc.cassandraTable("highschool", "students")
+    print("Result of count: ")
+    println(rdd.count)
+
+    sc.stop()
+  }
+}
+
+object CassandraReadDataframe {
+  def main(args: Array[String]) {
+    val conf = new SparkConf(true).set("spark.cassandra.connection.host", "127.0.0.1")
+
+    val sc = new SparkContext("local[4]", "Lighthouse", conf)
+
+    val sqlContext = new SQLContext(sc)
+
+    sqlContext.sql(
+      """CREATE TEMPORARY TABLE students
+        |USING org.apache.spark.sql.cassandra
+        |OPTIONS (
+        |  keyspace "highschool",
+        |  table "students",
+        |  cluster "Test Cluster",
+        |  pushdown "true"
+        |)""".stripMargin)
+
+    // Once tables have been registered, you can run SQL queries over them.
+    println("Result of SELECT *:")
+
+    val t0 = System.currentTimeMillis
+
+    val result = sqlContext.sql("SELECT * FROM students").collect
+
+    val t1 = System.currentTimeMillis
+
+    println("Elapsed time: " + (t1 - t0) + "ms")
+
+    result.foreach(println)
+
+    sc.stop()
+  }
+}
+
+object CassandraNative {
+  def main(args: Array[String]) {
+
+    val cluster: Cluster = Cluster.builder.addContactPoint("127.0.0.1").build
+
+    val session: Session = cluster.connect
+
+    val t0 = System.currentTimeMillis
+
+    val result = session.execute("SELECT * FROM highschool.students")
+
+    val t1 = System.currentTimeMillis
+
+    println("Elapsed time: " + (t1 - t0) + "ms")
+
+    println("Result of SELECT *:")
+
+    import scala.collection.JavaConversions._
+
+    result.all().foreach(println)
+
+    cluster.close()
+
+  }
 }
